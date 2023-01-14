3a7f4f801d75d9072f68160a047bede550741480
Alexandr Sachishin <sachishin1996@gmail.com>
2023-01-14 21:21:19 +0300

    feat: enhanced vite config to work with process env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import process from "process";
 
 defineProps<{ msg: string }>();
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+export default function applyConfig({ mode }) {
+  const ENV_PREFIX = "VITE_APP_";
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), ENV_PREFIX) };
+
+  return defineConfig({
+    plugins: [vue()],
+  });
+}
